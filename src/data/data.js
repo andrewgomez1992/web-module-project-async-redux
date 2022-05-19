db1fdf81f41b0d9e3a600867fb80d82409096763
Andrew Gomez <drewgomez209@gmail.com>
2022-05-19 11:31:16 -0700

    initial

---
+++ src/data/data.js
@@ -0,0 +1,100 @@
+const data = [{
+    "activity": "Learn how the internet works",
+    "type": "education",
+    "participants": 1,
+    "price": 0,
+    "link": "",
+    "key": "9414706",
+    "accessibility": 0.1
+},
+{
+    "activity": "Learn the Chinese erhu",
+    "type": "music",
+    "participants": 1,
+    "price": 0.6,
+    "link": "",
+    "key": "2742452",
+    "accessibility": 0.4
+},
+{
+    "activity": "Make a new friend",
+    "type": "social",
+    "participants": 1,
+    "price": 0,
+    "link": "",
+    "key": "1000000",
+    "accessibility": 0
+},
+{
+    "activity": "Organize your music collection",
+    "type": "busywork",
+    "participants": 1,
+    "price": 0,
+    "link": "",
+    "key": "3151646",
+    "accessibility": 0
+},
+{
+    "activity": "Start a daily journal",
+    "type": "relaxation",
+    "participants": 1,
+    "price": 0,
+    "link": "",
+    "key": "8779876",
+    "accessibility": 0
+},
+{
+    "activity": "Take a class at your local community center that interests you",
+    "type": "education",
+    "participants": 1,
+    "price": 0,
+    "link": "",
+    "key": "8750692",
+    "accessibility": 0.15
+},
+{
+    "activity": "Meditate for five minutes",
+    "type": "relaxation",
+    "participants": 1,
+    "price": 0,
+    "link": "",
+    "key": "3699502",
+    "accessibility": 0.05
+},
+{
+    "activity": "Learn Morse code",
+    "type": "education",
+    "participants": 1,
+    "price": 0,
+    "link": "https://en.wikipedia.org/wiki/Morse_code",
+    "key": "3646173",
+    "accessibility": 0
+},
+{
+    "activity": "Learn to sew on a button",
+    "type": "education",
+    "participants": 1,
+    "price": 0.05,
+    "link": "",
+    "key": "8731971",
+    "accessibility": 0.1
+},
+{
+    "activity": "Play a game of Monopoly",
+    "type": "social",
+    "participants": 4,
+    "price": 0.2,
+    "link": "",
+    "key": "1408058",
+    "accessibility": 0.3
+},
+{
+    "activity": "Play basketball with a group of friends",
+    "type": "social",
+    "participants": 5,
+    "price": 0,
+    "link": "",
+    "key": "8683473",
+    "accessibility": 0.7
+}
+]
